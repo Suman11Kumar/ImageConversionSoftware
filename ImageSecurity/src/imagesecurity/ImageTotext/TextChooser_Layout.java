41b823388f9774c0907620a3d0b58e0d0d23dbc2
max <max@max-PC>
2019-05-01 13:49:54 +0530

    Created card layout for history panel

---
+++ ImageSecurity/src/imagesecurity/ImageTotext/TextChooser_Layout.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imagesecurity.ImageTotext;
+
+import java.awt.Container;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author max
+ */
+public class TextChooser_Layout {
+
+    public void run(JPanel TextChooser, Container contentPane) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
